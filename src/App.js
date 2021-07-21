044af13fc52333a81e41d6ded02c3b2ba416e9b9
Gopan <gopan270@gmail.com>
2021-07-21 19:01:44 +0300

    tender:redeploy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import TenderCards from './TenderCards';
+import SwipeButtons from './SwipeButtons';
+
+function App() {
+  return (
+    <div className="app">
+     <Header/>
+     <TenderCards />
+     <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
